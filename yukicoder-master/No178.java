2bf26456aa2d57ecd15fa76db7410bb360f36567
tsunabit <tsuniki.miniki@gmail.com>
2019-05-19 23:41:33 +0900

    add

--- yukicoder-master/No178.java
+++ yukicoder-master/No178.java
@@ -7,16 +7,21 @@ public class No178 {
         // 標準入力から読み込む際に、Scannerオブジェクトを使う。
         Scanner s = new Scanner(System.in);
         int n = s.nextInt();
-        ArrayList<Integer> l = new ArrayList<Integer>();
+        ArrayList<Long> l = new ArrayList<Long>();
         
         // 𝑎+4𝑏+2𝑐
         for(int i = 0; i < n; i++) {
             // a + 4b + 2c
-        	l.add(s.nextInt() + (4 * s.nextInt()));
+        	Long temp = s.nextLong() + (4 * s.nextLong());
+        	l.add(temp);
         }
         Collections.sort(l, Collections.reverseOrder());
         long o = 0;
-        for(int v : l) {
+        for(Long v : l) {
+        	if((l.get(0) - v) % 2 != 0) {
+        		System.out.println(-1);
+        		return;
+        	}
         	o += l.get(0) - v;
         }
         if(o % 2 == 0) {
