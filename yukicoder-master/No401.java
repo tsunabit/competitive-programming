7dea13f13c384b23d5e76606475e2cfe3e0998fe
tsunabit <tsuniki.miniki@gmail.com>
2019-06-06 00:17:17 +0900

    add

--- yukicoder-master/No401.java
+++ yukicoder-master/No401.java
@@ -5,7 +5,8 @@ public class No401 {
     public static void main(String[] args) {
         // 標準入力から読み込む際に、Scannerオブジェクトを使う。
         Scanner s = new Scanner(System.in);
-        int n = s.nextInt();
+        // 配列と合わせるため-1する
+        int n = s.nextInt() - 1;
 //        初期値が0で進めなくなったら+1する変数𝑑を作って、𝑑を4で割った余りが
 //        0 ならば右に進む、
 //        1 ならば下に進む、
@@ -15,50 +16,55 @@ public class No401 {
         int d = 0;
         int count = 1;
         int gyou = 0, retu = 0;
-        int[][] masu = new masu[n][n];
-        while(count <= n * n) {
+        int[][] masu = new int[n + 1][n + 1];
+        while(count <= (n + 1) * (n + 1)) {
 //        	masu[gyou][retu] = count;
         	// 右へ移動
         	if(d % 4 == 0) {
-        		retu++;
         		masu[gyou][retu] = count;
+        		System.out.println("masu[" + gyou + "][" + retu + "] = " + count);
+        		retu++;
         		// dの判定
         		if(retu == n) {
         			d++;
-        		}else if(retu < n && masu[gyou][retu + 1] <> null) {
+        		}else if(retu < n && masu[gyou][retu + 1] != 0) {
         			d++;
         		}
         	// 下へ移動
         	}else if(d % 4 == 1) {
-        		gyou++;
         		masu[gyou][retu] = count;
+        		System.out.println("masu[" + gyou + "][" + retu + "] = " + count);
+        		gyou++;
         		// dの判定
         		if(gyou == n) {
         			d++;
-        		}else if(gyou < n && masu[gyou + 1][retu] <> null) {
+        		}else if(gyou < n && masu[gyou + 1][retu] != 0) {
         			d++;
         		}
         	// 左へ移動
         	}else if(d % 4 == 2) {
-        		gyou--;
         		masu[gyou][retu] = count;
+        		System.out.println("masu[" + gyou + "][" + retu + "] = " + count);
+        		retu--;
         		// dの判定
-        		if(gyou == 0) {
+        		if(retu == 0) {
         			d++;
-        		}else if(gyou > 0 && masu[gyou - 1][retu] <> null) {
+        		}else if(retu > 0 && masu[gyou][retu - 1] != 0) {
         			d++;
         		}
         	// 上へ移動
         	}else if(d % 4 == 4) {
-        		retu--;
         		masu[gyou][retu] = count;
+        		System.out.println("masu[" + gyou + "][" + retu + "] = " + count);
+        		gyou--;
         		// dの判定
-        		if(retu == 0) {
+        		if(gyou == 0) {
         			d++;
-        		}else if(retu > 0 && masu[gyou][retu - 1] <> null) {
+        		}else if(gyou > 0 && masu[gyou - 1][retu] != 0) {
         			d++;
         		}
         	}
+        	count++;
         }
         
         // 出力
