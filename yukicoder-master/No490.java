c65432207600630764b125a8ea6bc4644040c37e
tsunabit <tsuniki.miniki@gmail.com>
2019-06-11 23:55:32 +0900

    add

--- yukicoder-master/No490.java
+++ yukicoder-master/No490.java
@@ -1,25 +1,28 @@
 import java.util.*;
+import java.io.*;
 import java.math.*;
 
 public class No490 {
     public static void main(String[] args) {
-    	// 入力元がファイル
-    	File inputFile = new File("/Users/aa352872/Desktop/temp.txt");
-//    	Scanner sc = new Scanner(new File());
-    	Scanner sc = new Scanner(inputFile);
-        // 標準入力から読み込む際に、Scannerオブジェクトを使う。
-//        Scanner sc = new Scanner(System.in);
-        int n = Integer.parseInt(sc.nextLine());
-        String[] a = sc.nextLine().split(" ");
-        
-        int[] b = new int[n];
-        for(int i = 0; i < a.length; i++) {
-        	b[i] = Integer.parseInt(a[i]);
-        }
-        Arrays.sort(a);
-        for(int i = 0; i < n; i++) {
-        	System.out.print(b[i] + " ");
-        }
-        System.out.println("");
+    	try {
+    		// 入力元がファイル
+        	File inputFile = new File("/Users/aa352872/Desktop/temp.txt");
+        	Scanner sc = new Scanner(inputFile);
+            // 標準入力から読み込む際に、Scannerオブジェクトを使う。
+//            Scanner sc = new Scanner(System.in);
+            int n = Integer.parseInt(sc.nextLine());
+            String[] a = sc.nextLine().split(" ");
+            int[] b = new int[n];
+            for(int i = 0; i < a.length; i++) {
+            	b[i] = Integer.parseInt(a[i]);
+            }
+            Arrays.sort(b);
+            for(int i = 0; i < n; i++) {
+            	System.out.print(b[i] + " ");
+            }
+            System.out.println("");
+    	}catch(Exception e) {
+    		
+    	}
     }
 }
