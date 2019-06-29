1518f917dfa702fc81c19a04fc4497ad74e041cf
tsunabit <tsuniki.miniki@gmail.com>
2019-06-29 23:07:04 +0900

    update

--- yukicoder-master/No722.java
+++ yukicoder-master/No722.java
@@ -6,13 +6,13 @@ public class No722 {
     public static void main(String[] args) {
     	Scanner sc = new Scanner(System.in);
     	BigDecimal a = new BigDecimal(sc.nextInt()), b = new BigDecimal(sc.nextInt());
-    	BigDecimal j = new BigDecimal(10), z = new BigDecimal(0);
+    	BigDecimal j = new BigDecimal(10), z = new BigDecimal(0), m = new BigDecimal(-1), h = new BigDecimal(100);
     	
-    	if(a.remainder(j).equals(z) && b.remainder(j).equals(z)) {
+    	if(a.remainder(j).equals(z) && b.remainder(j).equals(z) && a.toString().length() > 2 && b.toString().length() > 2) {
     		System.out.println(a.multiply(b).divide(j));
     	}else {
     		BigDecimal min = new BigDecimal(-99999999), max = new BigDecimal(99999999);
-    		if(a.multiply(b).compareTo(min) > 0 && a.multiply(b).compareTo(max) < 0) {
+    		if(a.multiply(b).compareTo(min) >= 0 && a.multiply(b).compareTo(max) <= 0) {
     			System.out.println(a.multiply(b));
     		}else {
     			System.out.println("E");
