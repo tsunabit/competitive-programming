06af45a441674e9d5670770c0325fa6a792512b6
tsunabit <tsuniki.miniki@gmail.com>
2019-07-01 21:48:22 +0900

    update

--- yukicoder-master/No722.java
+++ yukicoder-master/No722.java
@@ -8,7 +8,8 @@ public class No722 {
     	BigDecimal a = new BigDecimal(sc.nextInt()), b = new BigDecimal(sc.nextInt());
     	BigDecimal j = new BigDecimal(10), z = new BigDecimal(0), m = new BigDecimal(-1), h = new BigDecimal(100);
     	
-    	if(a.remainder(j).equals(z) && b.remainder(j).equals(z) && a.toString().length() > 2 && b.toString().length() > 2) {
+    	if(a.abs().toString().replaceAll("0", "").length() == 1 && b.abs().toString().replaceAll("0", "").length() == 1 &&
+    	   a.remainder(j).equals(z) && b.remainder(j).equals(z) && a.toString().length() > 2 && b.toString().length() > 2) {
     		System.out.println(a.multiply(b).divide(j));
     	}else {
     		BigDecimal min = new BigDecimal(-99999999), max = new BigDecimal(99999999);
