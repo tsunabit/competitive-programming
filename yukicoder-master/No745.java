6012602e3f4f1d366c730dc7b38ee70f601781dd
tsunabit <tsuniki.miniki@gmail.com>
2019-07-07 23:11:11 +0900

    add

--- yukicoder-master/No745.java
+++ yukicoder-master/No745.java
@@ -6,50 +6,24 @@ public class No745 {
     public static void main(String[] args) {
     	Scanner sc = new Scanner(System.in);
     	int a = sc.nextInt(), b = sc.nextInt(), c = sc.nextInt(), d = sc.nextInt();
-    	int o = 0 , k = 0;
+    	int o = 0 , k = 50;
     	if(d >= 10) {
     		System.out.println("Impossible");
     		return;
     	}
     	for(int i = 0; i < b; i++) {
-    		if(i < 100) {
-    			o += 50;
-    		}else if (i < 200) {
-    			o += 100;
-    			k = 100;
-    		}else if (i < 300) {
-    			o += 200;
-    			k = 200;
-    		}else {
-    			o += 400;
-    			k = 400;
+    		for(int j = 0; j < (i / 100); j++) {
+    			k *= 2;
     		}
+    		o += k;
     	}
-    	if(k == 0) {
-    		for(int i = b; i < a + b; i++) {
-        		if(i < 100) o += 100;
-        		else if (i < 200) o += 200;
-        		else if (i < 300) o += 400;
-        		else o += 800;
-        	}
-    	}else {
-    		for(int i = b; i < a + b; i++) {
-    			if(i / 100 == b /100) {
-    				o += k;
-    			}else if(i / 100 == b /100 + 1) {
-    				o += k * 2;
-    			}else if(i / 100 == b /100 + 2) {
-    				o += k * 4;
-    			}else if(i / 100 == b /100 + 3) {
-    				o += k * 8;
-    			}
-//        		if(i < 100) o += 100;
-//        		else if (i < 200) o += 200;
-//        		else if (i < 300) o += 400;
-//        		else o += 800;
-        	}
+    	if(b < 100) k = 100;
+    	for(int i = b; i < a + b; i++) {
+    		for(int j = 0; j < (i / 100); j++) {
+    			k *= 2;
+    		}
+    		o += k;
     	}
-    	
     	System.out.println("Possible");
     	System.out.println(o);
     	
