8dfa4afa95a3fcdfd50a40248b1b5698fc67488b
tsunabit <tsuniki.miniki@gmail.com>
2019-07-09 23:22:50 +0900

    add

--- yukicoder-master/No769.java
+++ yukicoder-master/No769.java
@@ -10,45 +10,73 @@ public class No769 {
     	int[] h = new int[n];
     	String[] t = new String[m];
     	int c = 0;
-    	boolean rflag = false;
+    	int r = 1;
     	
     	for(int i = 0; i < m ; i++) {
     		t[i] = sc.next();
     	}
     	
     	for(int i = 0; i < m; i++) {
-    		if(c > n -1) {
-				c = c - n;
-			}else if(c < 0) {
-				c = c + n;
-			}
+//    		if(c > n -1) c = c - n;
+//			else if(c < 0) c = c + n;
+//    		h[c] += 1;
     		
-    		h[c] += 1;
     		if(i == m - 1) {
     			System.out.println((c + 1) + " " + h[c]);
     			return;
     		}
+    		
     		System.out.println("i = " + i + " c = " + (c + 1) + " maisu = " + h[c] + " fuda = " + t[i]);
+    		
     		if("number".equals(t[i])) {
-    			c++;
+    			c = c + (r * 1);
+    			c = count(c, n);
     		}else if("drawtwo".equals(t[i])) {
     			if(i < m - 2 && "drawtwo".equals(t[i + 1])) {
-    				c++;
+    				c = c + (r * 1);
+    				c = count(c, n);
     			}else {
-    				c += 2;
+    				c = c + (r * 2);
+    				c = count(c, n);
     			}
     		}else if("drawfour".equals(t[i])) {
     			if(i < m - 2 && "drawfour".equals(t[i + 1])) {
-    				c++;
+    				c = c + (r * 1);
+    				c = count(c, n);
     			}else {
-    				c += 2;
+    				c = c + (r * 2);
+    				c = count(c, n);
     			}
     		}else if("skip".equals(t[i])) {
-    			c += 2;
+    			c = c + (r * 2);
+    			c = count(c, n);
     		}else if("reverse".equals(t[i])) {
-    			c--;
+    			r = r * -1;
+    			c = c + (r * 1);
+    			c = count(c, n);
     		}
     	}
     	
+    	
+    	
     }
+    public static int count(int c, int n) {
+    	if(c > n -1) c = c - n;
+		else if(c < 0) c = c + n;
+    	return c;
+    }
+    
+//    public int count(int c, int n) {
+//    	if(c > n -1) c = c - n;
+//		else if(c < 0) c = c + n;
+//    	return c;
+//    }
+    
+//    public int maisu(String fuda) {
+//		int t = 0;
+//		if("drawtwo".equals(fuda)) t = -2; // h[c] -= 2;
+//		else if("drawfour".equals(fuda)) t = -4; //h[c] -= 4;
+//		else t = 1; //h[c] += 1;
+//		return t;
+//	}
 }
