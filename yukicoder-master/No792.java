690ff9a4ac5795bb8ae77218e48c468182a2f94c
tsunabit <tsuniki.miniki@gmail.com>
2019-07-12 22:02:22 +0900

    add

--- yukicoder-master/No792.java
+++ yukicoder-master/No792.java
@@ -6,9 +6,10 @@ public class No792 {
     public static void main(String[] args) {
     	Scanner sc = new Scanner(System.in);
     	int n = sc.nextInt();
-    	String o = "A=";
+    	StringBuilder sb = new StringBuilder();
+    	sb.append("A=");
     	int[][] q = new int[(int)Math.pow(2, n)][n+1];
-    	boolean f0 = false, f1 = false;
+    	int c = 0;
     	// ∧∨¬⊥⊤
     	for(int i = 0; i < Math.pow(2, n); i++) {
     		for(int j = 0; j < n + 1; j++) {
@@ -17,29 +18,26 @@ public class No792 {
     	}
     	
     	for(int i = 0; i < Math.pow(2, n); i++) {
-    			if(q[i][n] == 0) f0 = true;
-    			else f1 = true;
-    	}
-    	if(f0 && !f1) {
-    		System.out.println("A=⊥");
-    		return;
-    	}else if(!f0 && f1) {
-    		System.out.println("A=⊤");
-    		return;
-    	}
-    	
-    	for(int i = 0; i < Math.pow(2, n); i++) {
     		if(q[i][n] == 1) {
-    			o += "(";
+    			c++;
+    			sb.append("(");
     			for(int j = 0; j < n; j++) {
-    				if(q[i][j] == 1) o += "P_" + (j+1) + "∧";
-    				else o += "¬P_" + (j+1) + "∧";
+    				if(q[i][j] == 1)  sb.append("P_" + (j+1) + "∧");
+    				else sb.append("¬P_" + (j+1) + "∧");
         		}
-    			o += ")";
+    			sb.append(")");
+    			
     		}
     	}
-    	o = o.replaceAll("∧\\)\\(", "\\)∨\\(");
-    	o = o.replaceAll("∧\\)", "\\)");
-    	System.out.println(o);
+    	if(c == 0) {
+    		System.out.println("A=⊥");
+    	}else if(c == (int)Math.pow(2, n)) {
+    		System.out.println("A=⊤");
+    	}else {
+    		String str = "";
+    		str = sb.toString().replaceAll("∧\\)\\(", "\\)∨\\(");
+        	str = str.toString().replaceAll("∧\\)", "\\)");
+        	System.out.println(str);
+    	}
     }
 }
