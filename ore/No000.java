56dcf049d3d2fb3d15594b81642ab4347f717dfc
tsunabit <tsuniki.miniki@gmail.com>
2019-07-17 22:44:43 +0900

    add

--- ore/No000.java
+++ ore/No000.java
@@ -6,7 +6,9 @@ public class No000 {
     public static void main(String[] args) {
     	// Scanner sc = new Scanner(System.in);
     	System.out.println("factorial = " + factorial(9));
+    	System.out.println("fibonacchi = " + fibonacchi(20));
     }
+    
     // 階乗
     static int factorial(int n) {
     	if(n == 0) {
@@ -14,5 +16,13 @@ public class No000 {
     	}else {
     		return n * factorial(n - 1);
     	}
+    }
+    // フィボナッチ数列
+    static int fibonacchi(int n) {
+//    	System.out.println("call = " + n);
+    	if (n == 0 || n == 1) {
+            return n;
+        }
+        return fibonacchi(n - 1) + fibonacchi(n - 2);
     }
 }
