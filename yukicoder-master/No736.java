7e3b1379dd352a7b662fb9ffe62fd93893d61dd3
tsunabit <tsuniki.miniki@gmail.com>
2019-08-05 19:03:29 +0900

    add

--- yukicoder-master/No736.java
+++ yukicoder-master/No736.java
@@ -6,52 +6,60 @@ public class No736 {
     public static void main(String[] args) {
     	Scanner sc = new Scanner(System.in);
     	int n = sc.nextInt();
-    	int a[] = new int[n];
+    	long a[] = new long[n];
     	
     	for(int i = 0; i < n; i++) {
-    		a[i] = sc.nextInt();
+    		a[i] = sc.nextLong();
     	}
     	
-    	int max = 0;
+    	long max = 1;
     	for(int i = n-1; i > 0; i--) {
-    		int si = a[i];
-    		int bo = a[i - 1];
-    		int ama = -1;
+    		long si = a[i];
+    		long bo = a[i - 1];
+    		long ama = -1;
     		while(ama != 0) {
     			ama = si % bo;
     			if(ama == 0) {
     				break;
     			}else {
-    				bo = si;
+    				si = bo;
         			bo = ama;
     			}
     		};
-    		if(max == 0) {
+    		if(max == 1) {
+//    			System.out.println("max = " + max);
     			max = bo;
     		}else if(max < bo) {
+//    			System.out.println("max = " + max);
     			if(bo % max == 0) {
     				// nothing
     			}else {
-    				max = 0;
+    				max = 1;
     				break;
     			}
     		}else if(max > bo) {
     			if(max % bo == 0) {
     				max = bo;
     			}else {
-    				max = 0;
+    				max = 1;
     				break;
     			}
     		}
     		
     	}
-    	System.out.println("max = " + max);
+//    	System.out.println("max = " + max);
     	
-    	System.out.print(a[0]);
     	for(int i = 1; i < n; i++) {
-    		System.out.print(":" + a[i]);
+    		if(a[i]%max != 0) {
+    			max = 1;
+    		}
     	}
-    	
+    	System.out.println("max = " + max);
+    	System.out.print(a[0]/max);
+    	for(int i = 1; i < n; i++) {
+    		System.out.print(":" + a[i]/max);
+    	}
+    	System.out.println("");
     	
     }
 }
