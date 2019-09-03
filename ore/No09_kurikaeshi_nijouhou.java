99c5319bb5057fb1d235bac99e8b0c47e7fdbc56
tsunabit <tsuniki.miniki@gmail.com>
2019-09-03 16:35:21 +0900

    updated No09

--- ore/No09_kurikaeshi_nijouhou.java
+++ ore/No09_kurikaeshi_nijouhou.java
@@ -18,20 +18,18 @@ public class No09_kurikaeshi_nijouhou {
     	System.out.println("答え = " + pow(2, 9));
     }
     // 繰り返し2乗法
+    // 2^9、つまりx = 2, n = 9の場合
+    // 9 = 1001となり、2^9 = 2^1 * 2^8ということがわかる
+    // 繰り返し2乗法では、2^1 と 2^8を計算して2^9を算出する
     static public long pow(long x, long n) {
     	long ans = 1;
-    	int count = 0;
     	while(n > 0) {
-    		count++;
-    		System.out.println("count = " + count);
     		// n乗の数字を2進数に変換することがポイント
     		if((n & 1) == 1) {
-    			System.out.println("計算するx = " + x);
     			ans = ans * x;
     		}
     		// 2進数で考え、桁を上げるたびに底数をかけていく。一周する度にx, x^2, x^4, x^8となる
     		x = x * x;
-    		System.out.println("最新のx = " + x);
     		// bit全体を右に1つシフトして一番右を捨てる。つまり、割る2してるのと同じ
     		// ビットで1桁をずらす。「n = n >> 1」と同じ
     		n >>= 1;
