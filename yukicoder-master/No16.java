b200ca6d9a1238abcfab81ac32c55ffa26e5d374
tsunabit <tsuniki.miniki@gmail.com>
2019-09-03 17:09:22 +0900

    done No16

--- yukicoder-master/No16.java
+++ yukicoder-master/No16.java
@@ -8,26 +8,18 @@ public class No16 {
     	int x = sc.nextInt(), n = sc.nextInt();
     	long w = 1000003;
     	long total = 0;
-    	
     	for(int i = 0; i < n; i++) {
-    		//double t = Math.pow(x, sc.nextInt());
-    		double t = pow(x, sc.nextLong());
-//    		System.out.println(t);
+    		double t = pow(x, sc.nextLong(), w);
     		total += t;
-    		
     	}
-    	System.out.println("total = " + (total % w));
+    	System.out.println((total % w));
 	}
-	// 繰り返し2乗法
-    static public long pow(long x, long n) {
+    static public long pow(long x, long n, long w) {
     	long ans = 1;
     	while(n > 0) {
-    		if((n & 1) == 1) {
-    			ans = ans * x;
-    		}
-    		x = x * x;	// 2進数で考え、桁を上げるたびに底数をかけていく。一周する度にx, x^2, x^4, x^8となる
-    		System.out.println(x);
-    		n >>= 1;	// 桁をずらす。「n = n >> 1」と同じ
+    		if((n & 1) == 1) ans = ans * x % w;
+    		x = x * x % w;
+    		n >>= 1;
     	}
     	return ans;
     }
