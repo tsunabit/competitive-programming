80802065b667464d8906d29a45ff84b569551789
tsunabit <tsuniki.miniki@gmail.com>
2019-09-09 17:47:16 +0900

    done 44

--- yukicoder-master/No44.java
+++ yukicoder-master/No44.java
@@ -8,81 +8,12 @@ public class No44 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
-		dfs(n);
-		System.out.println("ans = " + ans);
-	}
-	
-	public static void dfs(int n) {
-		if(n == 0) {
-			ans++;
-			return;
+		long[] a = new long[n];
+		// フィボナッチ的なものをボトムアップで
+		for(int i = 0; i < 2; i++) a[i] = i+1;
+		for(int i = 2; i < n; i++) {
+			a[i] = a[i-1] + a[i-2];
 		}
-		int temp = n;
-		if(temp > 1) dfs(temp-2);
-		if(temp > 0) dfs(temp-1);
-		
-		return;
+		System.out.println(a[n-1]);
 	}
 }
-		
-//		
-//		
-//		Scanner sc = new Scanner(System.in);
-//		int n = Integer.parseInt(sc.nextLine());
-//		String[][] s = new String[n][n];
-//		for(int i = 0; i < n; i++) {
-//			s[i] = sc.nextLine().split("");
-//		}
-//		dfs(s, n);
-//		System.out.println(ans); 
-//	}
-//	
-//	public static void dfs(String[][] s, int n) {
-//		String[][] map1 = copy(s);
-//		for(int i = 0; i < n; i++) {
-//			for(int j = 0; j < n; j++) {
-//				if("-".equals(map1[i][j])) {
-//					map1[i][j] = "o";
-//					map1[j][i] = "x";
-//					dfs(map1, n);
-//					map1[i][j] = "x";
-//					map1[j][i] = "o";
-//					dfs(map1, n);
-//					return;
-//				}
-//			}
-//		}
-//		
-//		int[] kati = new int[n];
-//		for(int i = 0; i < n; i++) {
-//			for(int j = 0; j < n; j++) {
-//				if("o".equals(map1[i][j])) kati[i]++;
-//			}
-//		}
-//		
-//		int temp = kati[0];
-//		int r = 1;
-//		int ima = 99;
-//		Arrays.sort(kati);
-//		for(int i = n-1; i >= 0; i--) {
-//			if(temp < kati[i] && ima > kati[i]) {
-//				r++;
-//				ima = kati[i];
-//			}
-//		}
-//		if(ans > r) {
-//			ans = r;
-//		}
-//	}
-//	
-//	static String[][] copy(String[][] from){
-//		int aa = from[0].length;
-//		String[][] to = new String[aa][aa];
-//		for(int i=0;i<aa;i++){
-//			for(int j=0;j<aa;j++){
-//				to[i][j]=from[i][j];
-//			}
-//		}
-//		return to;
-//	}
-//}
