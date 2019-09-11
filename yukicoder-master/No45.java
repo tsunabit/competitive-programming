94c8174d8d4898fa75ef6cb3eaf67753d2356558
tsunabit <tsuniki.miniki@gmail.com>
2019-09-11 21:44:32 +0900

    done 45

--- yukicoder-master/No45.java
+++ yukicoder-master/No45.java
@@ -5,23 +5,14 @@ import java.math.*;
 public class No45 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int n = Integer.parseInt(sc.nextLine());
-		String[] vt = sc.nextLine().split(" ");
+		int n = sc.nextInt();
 		int[] v = new int[n];
-		for(int i = 0; i < n; i++) v[i] = Integer.parseInt(vt[i]);
+		for(int i = 0; i < n; i++) v[i] = sc.nextInt();
 		
-//		for(int i = 0; i < n; i++) System.out.println(v[i]);
-
-		int max = 0, temp = 0;
-		for(int i = 1; i < Math.pow(2, n); i++) {
-			temp = 0;
-			for (int j = 0; j < n; j++) {
-				if ((1 & i>>j) == 1) {
-					temp += v[j];
-				}
-			}
-			if(max < temp) max = temp;
-		}
-		System.out.println(max);
+		int[] r = new int[n];
+		r[0] = v[0];
+		if(n > 1) r[1] = Math.max(r[0], v[1]);
+		for(int i = 2; i < n; i++) r[i] = Math.max(r[i-1], r[i-2] + v[i]);
+		System.out.println(r[n-1]);
 	}
 }
