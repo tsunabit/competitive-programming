bb06ec2c563fa6394cd212950ff9e5c1f4d40c58
tsunabit <tsuniki.miniki@gmail.com>
2019-12-18 16:31:05 +0900

    done No55

--- yukicoder-master/No55.java
+++ yukicoder-master/No55.java
@@ -12,47 +12,41 @@ public class No55 {
 			x[i] = sc.nextInt();
 			y[i] = sc.nextInt();
 		}
+//		for(int i = 0; i < 3; i++) {
+//			System.out.println("x y = " + x[i] + " , " + y[i]);
+//		}
+		int x1, y1, x2, y2;
+		boolean f = false;
+		// 与えられた3点でのなす角を全て確認するため3回ループ
 		for(int i = 0; i < 3; i++) {
-			System.out.println("x y = " + x[i] + " , " + y[i]);
+			// ベクトル成分を考える。x0~x2のうち、x1を中心に考える。
+			x1 = x[1] - x[0];
+			y1 = y[1] - y[0];
+			x2 = x[2] - x[1];
+			y2 = y[2] - y[1];
+			// ベクトルの内積が0、かつ長さが等しい場合は正方形の対角線でないため、フラグを立ててループを抜ける
+			if((x1 * x2 + y1 * y2) == 0 && Math.pow(x1, 2) + Math.pow(y1, 2) == Math.pow(x2, 2) + Math.pow(y2, 2)) {
+				f = true;
+				break;
+			}
+			// 中心となる点を変更するため配列を入れ替える
+			// 入れ替えるのは2回のみでOK。最後の一回は不要。
+			if(i < 2) {
+				int tmpx = x[i];
+				x[i] = x[i+1];
+				x[i+1] = tmpx;
+				int tmpy = y[i];
+				y[i] = y[i+1];
+				y[i+1] = tmpy;
+			}
 		}
-		
-		
-//		
-//		int x1 = sc.nextInt();
-//		int y1 = sc.nextInt();
-//		int x2 = sc.nextInt();
-//		int y2 = sc.nextInt();
-//		int x3 = sc.nextInt();
-//		int y3 = sc.nextInt();
-//		
-//		double max = Math.sqrt(Math.pow(x2-x1, 2) + Math.pow(y2-y1, 2));	//t12
-//		double mid = Math.sqrt(Math.pow(x3-x1, 2) + Math.pow(y3-y1, 2));	//t13
-//		double min = Math.sqrt(Math.pow(x3-x2, 2) + Math.pow(y3-y2, 2));	//t23
-//		
-//		if(max < mid) {
-//			double t = max;
-//			max = mid;
-//			mid = t;
-//		}
-//		if(mid < min) {
-//			double t = mid;
-//			mid = min;
-//			min = t;
-//		}
-//		if(max < mid) {
-//			double t = max;
-//			max = mid;
-//			mid = t;
-//		}
-//		
-////		System.out.println(max);
-////		System.out.println(mid);
-////		System.out.println(min);
-////		System.out.println(min * Math.sqrt(2));
-//		
-//		if(mid == min && max == (min * Math.sqrt(2))) {
-//			
-//		}
-		
+		// 正方形が描ける場合は座標を出力
+		if(f) {
+			// 正方形の対角線の交点は、対角線の中点
+			// (x[0]+x[2])/2 , (y[0]+y[2])/2 = (x[1]+x?)/2 , (y[1]+y?)/2　を展開すると
+			System.out.println((x[0]+x[2]-x[1]) + " " + (y[0]+y[2]-y[1]));
+		}else {
+			System.out.println(-1);
+		}
 	}
 }
