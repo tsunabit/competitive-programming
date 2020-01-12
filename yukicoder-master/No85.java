889082d93229fe7d97575a8d4a38cb8fa5c00365
tsunabit <tsuniki.miniki@gmail.com>
2020-01-12 15:26:58 +0900

    updated no85

--- yukicoder-master/No85.java
+++ yukicoder-master/No85.java
@@ -3,8 +3,28 @@ import java.io.*;
 import java.math.*;
 
 public class No85 {
+	String = "NO";
+	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		int c = sc.nextInt();
+		int a[][] = new int[n][m];
+		// 0 = 未到達、 1 = 済
+		Arrays.fill(a, 0);
+	}
+	public static void DFS(int[][] a, int x, int y) {
+		if(x == 0) {
+			
+		}else if(x == m - 1) {
+			
+		}else if(y == 0) {
+			
+		}else if(y == n - 1) {
+			
+		}else {
+			
+		}
 	}
 }
