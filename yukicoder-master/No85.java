02b884898b0f9e12cc412a772279fb3188b4e293
tsunabit <tsuniki.miniki@gmail.com>
2020-01-28 19:45:58 +0200

    updated No85

--- yukicoder-master/No85.java
+++ yukicoder-master/No85.java
@@ -3,71 +3,34 @@ import java.io.*;
 import java.math.*;
 
 public class No85 {
-	String no = "NO";
-	static int M = 0;
-	static int N = 0;
-	static int C = 0;
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
 		int m = sc.nextInt();
 		int c = sc.nextInt();
-		M = m;
-		N = n;
-		C = c;
-//		int a[][] = new int[n][m];
-		int a[] = new int[n * m];
-		// 0 = 未到達、 1 = 済
-		Arrays.fill(a, 0);
-		int count = 0;
+		int[n*m] ary = new int[n*m]; 
+		Arrays.fill(ary, 0);
 	}
-	// x,y = 現在地
-	public static void DFS(int[] a, int c, int count) {
-		// 終了判定
-		if(count == M * N) {
-			if(a[C] == 0) {
-				System.out.println("YES");
-				return;
-			}
-		}
-		// 現在のボタン = (x − 1) * M + y
-		// int p = (x - 1) * M + y;
-		// int pamari = p % M;
-		int camari = c % M;
-		boolean left = true, right = true, ue = true, sita = true;
-		// 左右の判定
-		// 左端
-		if(pamari == 1) {
-			left = false;
-		// 右端
-		}else if(pamari == 0) {
-			right = false;
-		}
-		// 上下判定
-		// 上
-		if(p <= M) {
-			ue = false;
-		// 下
-		}else if((N * M - M + 1) <= p) {
-			sita = false;
-		}
+	
+	public static void dfs(int p, String[] ary) {
+		// 終了条件
 		
-		// DFS call
-		if(left && a[y][x-1] == 0) {
-			a[y][x-1] = 1;
-			DFS(a, x-1, y, count++);
+		// 移動条件判定
+		// 上
+		if((p-3) > 0 && ary[p-3] == 0) {
+			
 		}
-		if(right && a[y][x+1] == 0) {
-			a[y][x+1] = 1;
-			DFS(a, x+1, y, count++);
+		// 下
+		if((p+3) < n*m && ary[p+3] == 0) {
+			
 		}
-		if(ue && a[y-M][x] == 0) {
-			a[y-M][x] = 1;
-			DFS(a, x, y-M, count++);
+		// 左
+		if(p%m != 1 && ary[p-1] == 0) {
+			
 		}
-		if(ue && a[y+M][x] == 0) {
-			a[y+M][x] = 1;
-			DFS(a, x, y+M, count++);
+		// 右
+		if(p%m != 0 && ary[p+1] == 0) {
+			
 		}
 	}
 }
