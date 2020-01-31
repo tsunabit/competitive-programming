c74ffc00839d3938d3a21c1e02885782825fee17
tsunabit <tsuniki.miniki@gmail.com>
2020-01-31 12:54:14 +0200

    updated No85

--- yukicoder-master/No85.java
+++ yukicoder-master/No85.java
@@ -3,19 +3,39 @@ import java.io.*;
 import java.math.*;
 
 public class No85 {
+	static String ANS = "NO";
+	static int N;
+	static int M;
+	static int C;
+	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
 		int m = sc.nextInt();
 		int c = sc.nextInt();
-		int[n*m] ary = new int[n*m]; 
+		N = n;
+		M = m;
+		C = c;
+		
+		// int[] ary = new int[n*m];
+		// Integerにしないとaslist.containsが使えない
+		Integer[] ary = new Integer[n*m];
 		// 0 = 未達 1 = 到達済
 		Arrays.fill(ary, 0);
+		dfs(c, ary);
+		System.out.println(ANS);
 	}
 	
 	// p = 現在地
-	public static void dfs(int p, String[] ary) {
-		// 終了条件
+	public static void dfs(int p, Integer[] ary) {
+		System.out.println("開始確認");
+		// 終了条件 = 0を含まない 。つまり、全探索終了
+		if(!(Arrays.asList(ary).contains(0))){
+			if(p == C) {
+				ANS = "YES";
+				return;
+			}
+       }
 		
 		boolean ue = false, si = false, hi = false, mi = false;
 		// 移動条件判定
@@ -25,17 +45,17 @@ public class No85 {
 			ue = true;
 		}
 		// 下
-		if((p+3) < n*m && ary[p+3] == 0) {
+		if((p+3) < N*M && ary[p+3] == 0) {
 			ary[p+3] = 1;
 			si = true;
 		}
 		// 左
-		if(p%m != 1 && ary[p-1] == 0) {
+		if(p%M != 1 && ary[p-1] == 0) {
 			ary[p-1] = 1;
 			hi = true;
 		}
 		// 右
-		if(p%m != 0 && ary[p+1] == 0) {
+		if(p%M != 0 && ary[p+1] == 0) {
 			ary[p+1] = 1;
 			mi = true;
 		}
