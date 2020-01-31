23bb2acb09fef7a81b87a5cec47f4cee1d2f585c
tsunabit <tsuniki.miniki@gmail.com>
2020-01-31 21:16:06 +0200

    updated No85

--- yukicoder-master/No85.java
+++ yukicoder-master/No85.java
@@ -28,41 +28,69 @@ public class No85 {
 	
 	// p = 現在地
 	public static void dfs(int p, Integer[] ary) {
-		System.out.println("開始確認");
+		// 最初の確認
+//		System.out.println("開始確認 = " + p);
+//		for(int a : ary) System.out.print(a + " ");
+//		System.out.println("");
 		// 終了条件 = 0を含まない 。つまり、全探索終了
 		if(!(Arrays.asList(ary).contains(0))){
 			if(p == C) {
 				ANS = "YES";
+				// 最後の確認
+				System.out.println("終了確認 = " + p);
+				for(int a : ary) System.out.print(a + " ");
+				System.out.println("");
 				return;
 			}
        }
 		
-		boolean ue = false, si = false, hi = false, mi = false;
+//		boolean ue = false, si = false, hi = false, mi = false;
+		
 		// 移動条件判定
 		// 上
-		if((p-3) > 0 && ary[p-3] == 0) {
-			ary[p-3] = 1;
-			ue = true;
+		if((p-M) > 0 && ary[p-3-1] == 0) {
+//			System.out.println("ue = " + p + " = " + (p-3));
+			Integer[] ary2 = migula(ary);
+			ary2[p-3-1] = 1;
+//			ue = true;
+			dfs(p-3, ary2);
 		}
 		// 下
-		if((p+3) < N*M && ary[p+3] == 0) {
-			ary[p+3] = 1;
-			si = true;
+		if((p+M) < N*M && ary[p+3-1] == 0) {
+//			System.out.println("si = " + p + " = " + (p+3));
+			Integer[] ary2 = migula(ary);
+			ary2[p+3-1] = 1;
+//			si = true;
+			dfs(p+3, ary2);
 		}
 		// 左
-		if(p%M != 1 && ary[p-1] == 0) {
-			ary[p-1] = 1;
-			hi = true;
+		if(p%M != 1 && ary[p-1-1] == 0) {
+//			System.out.println("hi = " + p + " = " + (p-1));
+			Integer[] ary2 = migula(ary);
+			ary2[p-1-1] = 1;
+//			hi = true;
+			dfs(p-1, ary2);
 		}
 		// 右
-		if(p%M != 0 && ary[p+1] == 0) {
-			ary[p+1] = 1;
-			mi = true;
+		if(p%M != 0 && ary[p+1-1] == 0) {
+//			System.out.println("mi = " + p + " = " + (p+1));
+			Integer[] ary2 = migula(ary);
+			ary2[p+1-1] = 1;
+//			mi = true;
+			dfs(p+1, ary2);
 		}
 		
-		if(ue) dfs(p-3, ary);
-		if(si) dfs(p+3, ary);
-		if(hi) dfs(p-1, ary);
-		if(mi) dfs(p+1, ary);
+//		if(ue) dfs(p-3, ary);
+//		if(si) dfs(p+3, ary);
+//		if(hi) dfs(p-1, ary);
+//		if(mi) dfs(p+1, ary);
+		
+	}
+	public static Integer[] migula(Integer[] in) {
+		Integer[] out = new Integer[in.length];
+		for(int i = 0; i < in.length; i++) {
+			out[i] = in[i];
+		}
+		return out;
 	}
 }
