cb14c6c18a2f8d70b85736905011897b52eb5a47
tsunabit <tsuniki.miniki@gmail.com>
2020-02-10 18:01:51 +0200

    uypdated No92

--- yukicoder-master/No92.java
+++ yukicoder-master/No92.java
@@ -3,48 +3,76 @@ import java.io.*;
 import java.math.*;
 
 public class No92 {
+	static int K;
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
 		int m = sc.nextInt();
 		int k = sc.nextInt();
+		K = k;
 		
 		int[] a = new int[m * 3];
 		for(int i = 0; i < a.length; i++) {
 			a[i] = sc.nextInt();
 		}
-		
 		int[] d = new int[k];
 		for(int i = 0; i < k; i++) {
 			d[i] = sc.nextInt();
 		}
 //		System.out.println(Arrays.toString(a));
 //		System.out.println(Arrays.toString(d));
+		Set<Integer> set = new HashSet<Integer>();
+//		System.out.println("d.length = " + d.length);
 		
 		// 配列作成
 		for(int i = 2; i < a.length; i = i + 3) {
-			if(a[i] == a[a.length-1]) {
-//				System.out.println(a[i]);
-				ArrayList<Integer> al = new ArrayList<Integer>;
+			if(a[i] == d[d.length-1]) {
+				ArrayList<Integer> al = new ArrayList<Integer>();
 				al.add(a[i-1]);
 				al.add(a[i-2]);
+//				System.out.println(Arrays.toString(al.toArray()));
+				mo(a, al, d, 2, set);
+				
+				ArrayList<Integer> al2 = new ArrayList<Integer>();
+				al2.add(a[i-2]);
+				al2.add(a[i-1]);
+//				System.out.println(Arrays.toString(al2.toArray()));
+				mo(a, al2, d, 2, set);
 			}
 		}
-		Set<Integer> set = new HashSet<Integer>();
-		set.add(modoki(a, al, d, 1))
-	}
-	
-	public static int modoki(int[] a, ArrayList al,int[] d, int c) {
-		// terminate
-		if(k < c) {
-			return al.get(0);
-		}
+//		for(int ans : set) {
+//			System.out.print("ans = " + ans + " ");
+//		}
+//		System.out.print("");
+		int[] an = set.toArray();
+//		list.toArray(new String[list.size()]);
+		Arrays.sort(an);
+		System.out.println(Arrays.toString(an));
 		
+	}
+	public static void mo(int[] a, ArrayList<Integer> al,int[] d, int c, Set<Integer> set) {
+//		System.out.println("d.length - c = " + d.length + " - " + c);
+//		System.out.println("K = " + K);
+		// terminate
+		if(K < c) {
+			set.add(al.get(0));
+			return;
+		}
+		// 再起
 		for(int i = 2; i < a.length; i = i + 3) {
 			if(a[i] == d[d.length-c]) {
-				if(a[i-1] == al.get(al.lastIndexOf()-1)) {
-					al.add(a[i-2]);
-					modoki(a, al, d, c+1);
+				if(a[i-1] == (int)al.get(al.size()-1)) {
+					ArrayList<Integer> alt = new ArrayList<Integer>(al);
+					alt.add(a[i-1]);
+					alt.add(a[i-2]);
+//					System.out.println(Arrays.toString(alt.toArray()));
+					mo(a, alt, d, c+1, set);
+				}
+				if(a[i-2] == (int)al.get(al.size()-1)) {
+					ArrayList<Integer> alt = new ArrayList<Integer>(al);
+					alt.add(a[i-2]);
+					alt.add(a[i-1]);
+					mo(a, alt, d, c+1, set);
 				}
 			}
 		}
