f37b5e7e3564afecfa9871e9887246a381e2b437
tsunabit <tsuniki.miniki@gmail.com>
2020-03-01 09:34:05 +0200

    done No115

--- yukicoder-master/No115.java
+++ yukicoder-master/No115.java
@@ -3,54 +3,30 @@ import java.io.*;
 import java.math.*;
 
 public class No115 {
-	public static int max = 0;
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int d = sc.nextInt();
-		int k = sc.nextInt();
+		Scanner s = new Scanner(System.in);
+		int n = s.nextInt(), d = s.nextInt(), k = s.nextInt(), sum = 0;
+		int[] a = new int[k];
 		
-		int[] an = new int[n];
-		int dt = d;
-		int c = 0;
-		int t = 0;
-		while(c < n -1) {
-			for(int i = 0; i < k; i++) {
-				for(int j = c; j < n; j++) {
-					if(i == k-1) {
-						if((dt-j-1) == 0 && an[j] == 0) {
-							an[j]++;
-							dt -= j+1;
-							t++;
-							break;
-						}
-					}else {
-						if((dt-j-1) > 0 && an[j] == 0) {
-							an[j]++;
-							dt -= j+1;
-							t++;
-							break;
-						}
-					}
-				}
-			}
-			System.out.println(Arrays.toString(an));
-			c++;
-			if(t == k && dt == 0) {
-				for(int i = 0; i < n; i++) {
-					if(an[i] == 1) System.out.print((i+1) + " ");
-				}
-				System.out.println("");
-//				break;
-				return;
-			}else {
-				Arrays.fill(an, 0);
-				t = 0;
-				dt = d;
-			}
+		for(int i = 0; i < k; i++) {
+			a[i] = i + 1;
+			sum += i + 1;
 		}
-//		System.out.println(Arrays.toString(an));
-		System.out.println(-1);
-		
-	}
+		if(sum > d) {
+			System.out.println(-1);
+			return;
+		}
+		for(int i = 0; i < k; i++) {
+			sum -= a[a.length-1-i];
+			a[a.length-1-i]=Math.min(d-sum, n-i); 
+			sum += a[a.length-1-i];
+		}
+		if(sum == d) {
+			for(int i = 0; i < k; i++) {
+				System.out.print(a[i] + " ");
+			}
+		}else {
+			System.out.print(-1);
+		}
+ 	}
 }
