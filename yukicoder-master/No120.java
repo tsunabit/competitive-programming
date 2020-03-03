3acfcf543e0ec5150ef58025c7dadc84bf6490e4
tsunabit <tsuniki.miniki@gmail.com>
2020-03-03 10:44:41 +0200

    done No120

--- yukicoder-master/No120.java
+++ yukicoder-master/No120.java
@@ -4,45 +4,50 @@ import java.math.*;
 
 public class No120 {
 	public static void main(String[] args) {
-		Scanner s = new Scanner(System.in);
-		int t = s.nextInt();
+		try {
+			Scanner sc = new Scanner(System.in);
+			int t = sc.nextInt();
 
-		for(int i = 0; i < t; i++) {
-			int n = s.nextInt();
-
-			HashMap<Integer, Integer> m = new HashMap<Integer, Integer>();
-			for(int i = 0; i < n; i++) {
-				int t = s.nextInt();
-				if(m.containsKey(t)) {
-					m.put(t, m.get(t)+1);
-				}else {
-					m.put(t, 1);
+			for(int i = 0; i < t; i++) {
+				int n = sc.nextInt();
+				HashMap<Integer, Integer> m = new HashMap<Integer, Integer>();
+				
+				for(int j = 0; j < n; j++) {
+					int tmp = sc.nextInt();
+					if(m.containsKey(tmp)) {
+						m.put(tmp, m.get(tmp)+1);
+					}else {
+						m.put(tmp, 1);
+					}
 				}
-			}
-			int[] a = new int[m.size()];
-			int i = 0;
-			for(int c : m.keySet()) {
-				a[i] = m.get(c);
-				i++;
-			}
-			for(int j = 0; j < a.length; j++) {
-				System.out.println("hairetu = " + a[j]);
-			}
+				
+				for(int c : m.keySet()) {
+//					System.out.println("k : v = " + c + " : " + m.get(c));
+				}
+				
+				int[] a = new int[m.size()];
+				int z = 0;
+				for(int c : m.keySet()) {
+					a[z] = m.get(c);
+					z++;
+				}
+				Arrays.sort(a);
+//				System.out.println(Arrays.toString(a));
 
-//				int[] a = new int[100];
-//				
-//				
-//				Arrays.fill(a, 0);
-//				for(int i = 0; i < n; i++) a[s.nextInt()-1]++;
-//				long ss = 0;
-//				for(int i = 0; i < a.length; i++) {
-//					for(int j = i+1; j < a.length; j++) {
-//						for(int k = j+1; k < a.length; k++) {
-//							ss += (long)a[i] * a[j] * a[k] % mod;
-//						}
-//					}
-//				}
-//				System.out.println(ss % mod);
-		}
+				long ss = 0;
+				while(a.length-3 >= 0 && a[a.length-3] > 0) {
+					ss++;
+					a[a.length-1]--;
+					a[a.length-2]--;
+					a[a.length-3]--;
+					Arrays.sort(a);
+				}
+				System.out.println(ss);
+			}
+		} catch(Exception e) {
+    		System.out.println("例外が発生しました。");
+            System.out.println(e);
+            return;
+    	}
  	}
 }
