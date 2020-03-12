786bfbbb222318aebc03cb8400ba71ca30518e89
tsunabit <tsuniki.miniki@gmail.com>
2020-03-12 12:47:06 +0200

    done No197

--- yukicoder-master/No197.java
+++ yukicoder-master/No197.java
@@ -11,7 +11,30 @@ public class No197 {
 		String[] a = new String[3];
 		a = sc.next().split("");
 		
-		System.out.println(b.toString());
-		System.out.println(a.toString());
+		int ac = 0, bc = 0;
+		for(int i = 0; i < 3; i++) {
+			if(a[i].equals("o")) ac++;
+			if(b[i].equals("o")) bc++;
+		}
+		if(ac != bc) {
+			System.out.println("SUCCESS");
+			return;
+		}
+		if(n == 0) {
+			if(a[0].equals(b[0]) && a[1].equals(b[1]) && a[2].equals(b[2])) {
+				System.out.println("FAILURE");
+			}else {
+				System.out.println("SUCCESS");
+			}
+		}else if(n == 1) {
+			if((a[0].equals(b[1]) && a[1].equals(b[0]) && a[2].equals(b[2])) || 
+				(a[0].equals(b[0]) && a[1].equals(b[2]) && a[2].equals(b[1]))) {
+				System.out.println("FAILURE");
+			}else {
+				System.out.println("SUCCESS");
+			}
+		}else {
+			System.out.println("FAILURE");
+		}
 	}
 }
