b4866beb2cdd2bc9f8745e23dbf99410f5d48f05
tsunabit <tsuniki.miniki@gmail.com>
2020-03-16 11:00:09 +0200

    done No212

---
+++ yukicoder-master/No212.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+public class No212 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int p = sc.nextInt(), c = sc.nextInt();
+		System.out.println(Math.pow(41.0/6.0, p) * Math.pow(49.0/6.0, c));
+	}
+}
